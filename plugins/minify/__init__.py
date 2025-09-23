2c3b0a69d06efb4dc29ac73c82c425681fba9582
Lucas Malizia <131050418+0xlukem@users.noreply.github.com>
2025-09-23 10:06:58 +0100

    Apply suggestions from code review
    Co-authored-by: Erin Shaben <eshaben@icloud.com>

--- plugins/minify/__init__.py
+++ plugins/minify/__init__.py
@@ -2,9 +2,7 @@
 MkDocs plugin to minify HTML, JS or CSS files prior to being written to disk.
 
 This is a fork of mkdocs-minify-plugin by Byrne Reese and Lars Wilhelmer.
-Maintained by Lucas Malizia.
+Maintained by PaperMoon.
 """
 
 __version__ = "0.8.0"
-__author__ = "Lucas Malizia"
-__email__ = "lucas.malizia27@gmail.com"
